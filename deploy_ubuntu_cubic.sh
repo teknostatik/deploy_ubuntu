7337a2cea94453839abfecda36ba726f9dbe253e
Andy Ferguson <andy@teknostatik.org>
2021-05-24 20:33:21 +0100

    Refreshed wallpaper

--- deploy_ubuntu_cubic.sh
+++ deploy_ubuntu_cubic.sh
@@ -63,12 +63,11 @@ sudo mv config /etc/i3/
 mkdir /usr/share/wallpaper
 cd /usr/share/wallpaper
 sudo wget https://www.dropbox.com/s/0yg8txbgw0ifqmg/9dy0gvxq7fl61.png
-sudo wget https://www.dropbox.com/s/mtlzx6v4hlf8x2p/73hkbyuzk0p41.png
 sudo wget https://www.dropbox.com/s/j9mmfedrc8r9zba/231-2311974_big.jpg
-sudo wget https://www.dropbox.com/s/nlvt4x7n8yuxnd2/alena-aenami-away-1k.jpg
-sudo wget https://www.dropbox.com/s/ujqf1rpmdphcs1x/uwp825694.jpeg
-sudo wget https://www.dropbox.com/s/c8n7m3i2ruc84br/115104751-1a10a380-9f20-11eb-95b0-8950ac6381f0.png
 sudo wget https://www.dropbox.com/s/cljxhezhxuu3nce/background.png
+sudo wget https://www.dropbox.com/s/f2rkmbv13c8t769/1920x1080-dark-linux.png
+sudo wget https://www.dropbox.com/s/1i7g8u5h6whd5dv/3430638.png
+sudo wget https://www.dropbox.com/s/bkae9ethe3jqbod/jyrji9bnp3171.jpg
 cd $HOME
 wget https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wallpaper
 sudo mv randomise_wallpaper /usr/local/bin/
