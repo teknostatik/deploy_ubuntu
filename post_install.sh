d5e08d477e17bbebe0ff812f684c2f8a9edce510
Andy Ferguson <andy@teknostatik.org>
2021-06-07 16:17:06 +0100

    Getting rid of things I don't use

--- post_install.sh
+++ post_install.sh
@@ -26,9 +26,7 @@ sudo protonvpn init
 echo "alias ls='ls -la'" >> .bashrc
 echo "alias mp='multipass list'" >> .bashrc
 echo "alias top='bpytop'" >> .bashrc
-echo "alias ls='ls -la'" >> .zshrc
-echo "alias mp='multipass list'" >> .zshrc
-echo "alias top='bpytop'" >> .zshrc
+
 
 # Build a container running the latest LTS for testing things on
 #  This will work on machines with 8GB of ram without destroying your system, despite what it suggests
