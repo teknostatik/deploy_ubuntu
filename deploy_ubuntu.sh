222a38c425f9a9344c66bef90eefb4d54b10050a
Andy Ferguson <andy@teknostatik.org>
2021-09-19 20:10:21 +0100

    Added needrestart

--- deploy_ubuntu.sh
+++ deploy_ubuntu.sh
@@ -18,7 +18,7 @@ sudo apt -y upgrade
 
 # Install some basic utilities
 
-sudo apt install -y git curl scrot byobu synaptic xautolock shellcheck barrier kitty zathura pcmanfm lxde featherpad tasksel inxi
+sudo apt install -y git curl scrot byobu synaptic xautolock shellcheck barrier kitty zathura pcmanfm lxde featherpad tasksel inxi needrestart
 sudo add-apt-repository ppa:agornostal/ulauncher -y
 sudo apt install -y ulauncher
 
