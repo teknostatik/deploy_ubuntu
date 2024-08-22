e225b02518f1453bdbf95e06f4795c715db73c00
Andy Ferguson <andy@teknostatik.org>
2024-08-22 20:51:55 +0100

    Significant rewrite

--- deploy_ubuntu.sh
+++ deploy_ubuntu.sh
@@ -28,59 +28,33 @@ sudo apt -y upgrade
 
 # Install some basic utilities
 
-sudo apt install -y git curl scrot byobu synaptic xautolock shellcheck barrier kitty zathura pcmanfm lxde featherpad tasksel inxi needrestart htop apt-transport-https blueman ubuntu-restricted-extras flameshot
-
-# Install and configure i3
-
-wget https://raw.githubusercontent.com/teknostatik/i3_config/main/install_i3.sh
-sudo mv install_i3.sh /usr/local/bin/
-sudo chmod 755 /usr/local/bin/install_i3.sh
-install_i3.sh
-
-# Install some snaps
-
-sudo snap install multipass --classic
-sudo snap install unixbench
-sudo snap install tube-converter
-
-# Install Flatpak
-
-sudo apt install -y flatpak gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# install vscode
-
-# flatpak install flathub com.visualstudio.code -y
-
-# or
-
-sudo apt-get install gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt update
-sudo apt install code
-
-# Install everything needed for Tor
-
-sudo apt install -y torbrowser-launcher onionshare
-
-# Set up git
-
-git config --global user.name "Andy Ferguson"
-git config --global user.email "andy@teknostatik.org"
-
-# install ProtonVPN
-
-wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb
-sudo dpkg -i ./protonvpn-stable-release_1.0.3-3_all.deb
-sudo apt update
-sudo apt install -y proton-vpn-gnome-desktop
-
-# Install Zerotier
-
-curl -s https://install.zerotier.com | sudo bash
+sudo apt install -y \
+    htop \
+    git \
+    byobu \
+    synaptic \
+    xautolock \
+    shellcheck \
+    xinit \
+    zathura \
+    network-manager-gnome \
+    rsync \
+    curl \
+    ttf-mscorefonts-installer \
+    build-essential \
+    gimp \
+    rhythmbox \
+    vlc \
+    brasero \
+    sound-juicer \
+    lxappearance \
+    flameshot \
+    pandoc \
+    texlive \
+    texlive-latex-extra \
+    abiword \
+    ubuntu-restricted-extras \
+    remmina
 
 # Download and install a custom update script
 
@@ -90,33 +64,144 @@ sudo chmod 755 /usr/local/bin/updateall
 
 # Install some packages to make remote shells more interesting and then add them to the profile for the logged in user
 
-wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.8.7/fastfetch-linux-amd64.deb
-sudo dpkg -i fastfetch-linux-amd64.deb
-sudo apt install -y fortune-mod cowsay
-echo "echo; fortune | cowsay;echo" >> .profile
-echo "echo; fastfetch;echo" >> .profile
+## Define variables
+FF_VERSION="2.21.0"
+FF_URL="https://github.com/fastfetch-cli/fastfetch/releases/download/${FF_VERSION}/fastfetch-linux-amd64.deb"
+TEMP_DEB="$(mktemp)" # Create a temporary file for the .deb download
 
-# Install the applications I use for writing, editing and previewing text
+## Download and install Fastfetch
+wget -qO "$TEMP_DEB" "$FF_URL"
+sudo dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB" # Clean up temporary .deb file
 
-sudo apt install -y pandoc texlive texlive-latex-extra abiword
+## Install fortune and cowsay
+sudo apt-get update
+sudo apt-get install -y fortune-mod cowsay
 
-# Install some desktop applications for creating, editing and playing common media types
-# Some of these are quite large so you might want to comment them out
-
-sudo apt install -y gimp rhythmbox vlc brasero sound-juicer transmission
+## Add commands to .profile if they don't already exist
+PROFILE="$HOME/.profile"
+grep -qxF 'echo; fortune | cowsay; echo' "$PROFILE" || echo 'echo; fortune | cowsay; echo' >> "$PROFILE"
+grep -qxF 'echo; fastfetch; echo' "$PROFILE" || echo 'echo; fastfetch; echo' >> "$PROFILE"
 
 # Add some aliases
 
-echo "alias ls='ls -la'" >> .bashrc
-echo "alias mp='multipass list'" >> .bashrc
-echo "alias top='htop'" >> .bashrc
+echo "alias ls='ls -la'" >> /home/$USER/.bashrc
+echo "alias top='htop'" >> /home/$USER/.bashrc
 
-# Install drivers for Displaylink docking stations, such as the lenovo and Dell ones I use at home and work
+# Configure git
+echo "We are now going to configure git"
+read -p "Enter your full name: " fullname
+read -p "Enter your email address: " email
+git config --global user.name "$fullname"
+git config --global user.email "$email"
 
-git clone https://github.com/AdnanHodzic/displaylink-debian.git
-cd displaylink-debian
-sudo ./displaylink-debian.sh
-cd ..
+# Display the configured settings for git
+echo "Git has been configured with the following details:"
+git config --global --get user.name
+git config --global --get user.email
+
+# Some optional packages, which users can choose to install
+
+# Function to install vscode
+install_vscode() {
+    sudo apt-get install -y gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/keyrings/packages.microsoft.gpg > /dev/null
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    sudo apt-get install -y apt-transport-https
+    sudo apt-get update
+    sudo apt-get install -y code
+}
+
+# Function to install and configure i3
+install_i3() {
+    sudo apt-get install -y i3 i3blocks feh arandr scrot xautolock barrier kitty imagemagick polybar caffeine copyq picom blueman pcmanfm
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/config -O /tmp/i3_config
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/lock.sh -O /usr/local/bin/lock.sh
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/kitty.conf -O /tmp/kitty.conf
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/picom.conf -O /tmp/picom.conf
+    mkdir -p /home/$USER/.config/i3 /home/$USER/.config/kitty /home/$USER/.config/picom
+    mv /tmp/i3_config /home/$USER/.config/i3/config
+    mv /tmp/kitty.conf /home/$USER/.config/kitty/kitty.conf
+    mv /tmp/picom.conf /home/$USER/.config/picom/picom.conf
+    sudo install -D -o root -g root -m 755 /usr/local/bin/lock.sh /usr/local/bin/lock.sh
+    sudo mkdir -p /usr/share/wallpaper
+    sudo cp -R /usr/share/backgrounds/* /usr/share/wallpaper
+    wget -q https://raw.githubusercontent.com/teknostatik/i3_config/main/randomise_wallpaper -O /usr/local/bin/randomise_wallpaper
+    sudo chmod 755 /usr/local/bin/randomise_wallpaper
+}
+
+# Function to install tor
+install_tor() {
+    sudo apt-get install -y torbrowser-launcher onionshare
+}
+
+# Function to install DisplayLink
+install_displaylink() {
+    git clone https://github.com/AdnanHodzic/displaylink-debian.git /tmp/displaylink-debian
+    echo "Do not reboot when given the option. You will need to reboot before trying to use your docking station."
+    sudo /tmp/displaylink-debian/displaylink-debian.sh
+    #wget -q https://raw.githubusercontent.com/teknostatik/debian/master/20-displaylink.conf -O /etc/X11/xorg.conf.d/20-displaylink.conf
+    rm -rf /tmp/displaylink-debian
+}
+
+# Function to install flatpak
+install_flatpak() {
+    sudo apt-get install -y flatpak gnome-software-plugin-flatpak
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+}
+
+# Function to install ProtonVPN
+install_protonvpn() {
+    wget -q https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb -O /tmp/protonvpn.deb
+    sudo dpkg -i /tmp/protonvpn.deb
+    sudo apt-get update
+    sudo apt-get install -y proton-vpn-gnome-desktop
+    rm /tmp/protonvpn.deb
+}
+
+# Function to install Zerotier 
+install_zerotier() {
+    curl -s https://install.zerotier.com | sudo bash
+}
+
+# Function to install Unixbench
+install_unixbench() {
+    sudo apt install -y libx11-dev libgl1-mesa-dev libxext-dev perl perl-modules make git
+    git clone https://github.com/kdlucas/byte-unixbench.git
+# uncomment tne next 2 lines to run the benchmark now
+# cd byte-unixbench/UnixBench/
+# ./Run
+}
+
+# Fuction to install Dropbox
+install_dropbox() {
+    sudo apt install -y nautilus-dropbox
+    dropbox start -i
+}
+
+# Prompt function
+prompt_install() {
+    read -p "Do you want to install $1? (yes/no): " choice
+    if [[ "$choice" == "yes" ]]; then
+        $2
+    fi
+}
+
+# Main script to prompt user and call installation functions
+prompt_install "Visual Studio Code" install_vscode
+prompt_install "i3 tiling window manager" install_i3
+prompt_install "Tor browser and Onionshare" install_tor
+prompt_install "DisplayLink docking station support" install_displaylink
+prompt_install "flatpak" install_flatpak
+prompt_install "ProtonVPN" install_protonvpn
+prompt_install "Zerotier" install_zerotier
+prompt_install "Unixbench" install_unixbench
+prompt_install "Dropbox" install_dropbox
+
+# Install some snaps
+
+sudo snap install multipass --classic
+sudo snap install tube-converter
 
 # Build a container running the latest LTS for testing things on
 # This will work on a machine with 8GB of RAM, despite what it looks like
@@ -135,10 +220,5 @@ multipass set client.primary-name=ubuntu-lts
 mkdir ~/.ssh/multipassKey
 sudo cp /var/snap/multipass/common/data/multipassd/ssh-keys/id_rsa ~/.ssh/multipassKey/id_rsa
 sudo chown $USER -R ~/.ssh/multipassKey
-
-# Download and install Dropbox
-
-sudo apt install -y nautilus-dropbox
-dropbox start -i
 
 echo "The script has now finished running."
