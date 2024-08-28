b69624bb3a85c989fc55a74435d22c7797164c28
Andy Ferguson <andy@teknostatik.org>
2024-08-28 19:33:32 +0100

    Added fastfetch and some code omtimisation

--- deploy_ubuntu_wsl.sh
+++ deploy_ubuntu_wsl.sh
@@ -2,9 +2,9 @@
 
 # Script to run afer installing Ubuntu WSL from the Windows app store. Comment out any sections that don't interest you.
 # Also works with Multipass (normal containers, not Ubuntu Core), although I have a better script for this now.
-echo "------------------------------------------------------------------"
-echo "Ubuntu configuration script for WSL and Multipass - v0.4, May 2021"
-echo "------------------------------------------------------------------"
+echo "-------------------------------------------------"
+echo "Ubuntu configuration script for WSL and Multipass"
+echo "-------------------------------------------------"
 
 # Update software
 
@@ -12,7 +12,7 @@ sudo apt update
 
 # Install some basic utilities
 
-sudo apt install -y byobu htop
+sudo apt install -y byobu htop curl wget
 
 # Download a custom update script and then use it to get software updates and do some cleaning up
 
@@ -21,13 +21,32 @@ sudo mv updateall /usr/local/bin/
 sudo chmod 755 /usr/local/bin/updateall
 updateall
 
-# Install some packages to make remote shells more interesting and then add them to the profile
+## Install some packages to make remote shells more interesting and then add them to the profile for the logged in user
 
-sudo apt install -y neofetch fortune-mod cowsay
-echo "echo; fortune | cowsay;echo" >> .profile
-echo "echo; neofetch;echo" >> .profile
+## Define variables
+FF_VERSION="2.21.0"
+FF_URL="https://github.com/fastfetch-cli/fastfetch/releases/download/${FF_VERSION}/fastfetch-linux-amd64.deb"
+TEMP_DEB="$(mktemp)" # Create a temporary file for the .deb download
 
-# Set some aliases
+## Download and install Fastfetch
+wget -qO "$TEMP_DEB" "$FF_URL"
+sudo dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB" # Clean up temporary .deb file
 
-echo "alias ls='ls -la'" >> .bashrc
-echo "alias top='htop'" >> .bashrc
+## Install fortune and cowsay
+sudo apt-get update
+sudo apt-get install -y fortune-mod cowsay
+
+## Add commands to .profile if they don't already exist
+PROFILE="$HOME/.profile"
+grep -qxF 'echo; fortune | cowsay; echo' "$PROFILE" || echo 'echo; fortune | cowsay; echo' >> "$PROFILE"
+grep -qxF 'echo; fastfetch; echo' "$PROFILE" || echo 'echo; fastfetch; echo' >> "$PROFILE"
+
+# Add some aliases
+
+echo "alias ls='ls -la'" >> /home/$USER/.bashrc
+echo "alias top='htop'" >> /home/$USER/.bashrc
+
+# Install zerotier
+
+curl -s https://install.zerotier.com | sudo bash
