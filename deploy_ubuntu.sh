71c4d6504e1b7b00327223da2f245280efc7bdc2
Andy Ferguson <andy@teknostatik.org>
2024-09-13 20:02:54 +0100

    Several bugfixes

--- deploy_ubuntu.sh
+++ deploy_ubuntu.sh
@@ -130,7 +130,7 @@ install_flatpak() {
 
 # Function to install ProtonVPN
 install_protonvpn() {
-    wget -q https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb -O /tmp/protonvpn.deb
+    wget -q https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.4_all.deb -O /tmp/protonvpn.deb
     sudo dpkg -i /tmp/protonvpn.deb
     sudo apt-get update
     sudo apt-get install -y proton-vpn-gnome-desktop
@@ -160,20 +160,6 @@ install_dropbox() {
 # Function to install Multipass
 install_multipass() {
     sudo snap install multipass --classic
-    # Build a container running the latest LTS for testing things on
-    # This will work on a machine with 8GB of RAM, despite what it looks like
-    multipass launch -m 8G -d 20G lts --name ubuntu-lts
-    multipass exec ubuntu-lts -- wget https://raw.githubusercontent.com/teknostatik/deploy_ubuntu/main/deploy_ubuntu_wsl.sh
-    multipass exec ubuntu-lts -- sudo mv deploy_ubuntu_wsl.sh /usr/local/bin/
-    multipass exec ubuntu-lts -- sudo chmod 755 /usr/local/bin/deploy_ubuntu_wsl.sh
-    multipass exec ubuntu-lts -- deploy_ubuntu_wsl.sh
-    multipass stop ubuntu-lts
-    multipass set client.primary-name=ubuntu-lts
-    # Enable that container (and any future ones) to run graphical apps
-    # Syntax is ssh -X -i ~/.ssh/multipassKey/id_rsa ubuntu@container_ip_address
-    mkdir ~/.ssh/multipassKey
-    sudo cp /var/snap/multipass/common/data/multipassd/ssh-keys/id_rsa ~/.ssh/multipassKey/id_rsa
-    sudo chown $USER -R ~/.ssh/multipassKey
 }
 
 # Fuctiona to install Parabolic
@@ -214,12 +200,12 @@ prompt_install() {
 prompt_install "Visual Studio Code" install_vscode
 prompt_install "i3 tiling window manager" install_i3
 prompt_install "Tor browser and Onionshare" install_tor
-prompt_install "DisplayLink docking station support" install_displaylink
+#prompt_install "DisplayLink docking station support" install_displaylink
 prompt_install "Flatpak" install_flatpak
 prompt_install "ProtonVPN" install_protonvpn
 prompt_install "Zerotier" install_zerotier
 prompt_install "Unixbench" install_unixbench
-prompt_install "Multipass, with a default container" install_multipass
+prompt_install "Multipass" install_multipass
 prompt_install "Dropbox" install_dropbox
 prompt_install "Parabolic" install_parabolic
 prompt_install "and configure Git" install_git
